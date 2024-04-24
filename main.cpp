51d806ed1f82697e86bd81f14668e02a8d40c1d0
Patryk <166824732+PatKrusz@users.noreply.github.com>
2024-04-24 22:12:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 namespace py = pybind11;
 namespace mp = matplot;
 
+//testowa funkcja do usunięcia po prostu sprawdzałem czy się kompiluje jak należy
 float square(float x) { return x * x; }
-
+//////////////////////////////////////
 PYBIND11_MODULE(projekt, m) {
+    //również do usunięcia póki co zostawiam jako template
     m.def("square", &square);
+    ///////////////////////////////////
 }
