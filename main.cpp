bceeab9d17b4cc2ffab2cf40c9c951ed89f088aa
LiquidLaskowicz <mixerak123@gmail.com>
2024-04-26 16:39:12 +0200

    matplotplusplus functionality test

--- main.cpp
+++ main.cpp
@@ -50,11 +50,26 @@ int test_generowania_sin()
 
 
 
-PYBIND11_MODULE(projekt, m) {
-    //również do usunięcia póki co zostawiam jako template
-    m.def("square", &square);
-    ///////////////////////////////////
+int wykres() {
+	using namespace matplot;
+	std::vector<double> x = linspace(0, 2 * pi);
+	std::vector<double> y = transform(x, [](auto x) { return sin(x); });
+
+	plot(x, y, "-o");
+	hold(on);
+	plot(x, transform(y, [](auto y) { return -y; }), "--xr");
+	plot(x, transform(x, [](auto x) { return x / pi - 1.; }), "-:gs");
+	plot({ 1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1 }, "k");
+
+	show();
+	return 0;
 }
 
 
 
+PYBIND11_MODULE(projekt, m) {
+	//również do usunięcia póki co zostawiam jako template
+	m.def("square", &square);
+	///////////////////////////////////
+	m.def("wykres", &wykres);
+}
